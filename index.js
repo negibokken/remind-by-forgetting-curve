ee1a761374f678fc64e10b4f1019cbd2d3656a79
Ryo Ogawa <negibokken@gmail.com>
2022-04-03 12:54:59 +0900

    Update input arguments

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+const defaultRemindDays = [1, 3, 10, 30, 90];
+const defaultMessage = 'It' s time to remember this issue!'
+
+    try {
+  const labels = core.getInput('labels');
+  const remindDays = core.getInput('remind-days') || defaultRemindDays;
   const time = (new Date()).toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const message = core.getInput('message') || defaultMessage;
+  console.log(`${time} ${message}`)
+  console.log(`${labels}`)
+  console.log(`${remindDays}`)
 } catch (error) {
   core.setFailed(error.message);
 }
